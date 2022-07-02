0a41bd05cd3678df09fcba43fa6b8cf3566e6369
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2022-07-02 12:08:29 +0200

    Add pred feature

--- R/linearReg.R
+++ R/linearReg.R
@@ -1,8 +1,19 @@
 #' Applies linear regression and finds the coefficient values
 #' @export
+#' @importFrom stats lm
 #' @param x input values
 #' @param y output values
 linearReg <- function(x, y) {
   relation <- lm(y~x)
   relation
+}
+
+#' Predict outputs of the given x values using the given model
+#' @export
+#' @importFrom stats predict
+#' @param model input model
+#' @param x input values
+linearPred <- function(model, x) {
+  result <- predict(model, x)
+  result
 }
